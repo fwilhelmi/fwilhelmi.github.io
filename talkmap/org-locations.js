b94b6b247d8f3c58a7d829460ee52cdf6fd17ada
Francisco Wilhelmi <francisco.wilhelmi@upf.edu>
2018-06-13 09:52:29 +0200

    Update of "Talks"
    Update of "Talks"

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Barcelona, Spain",
+    41.40406248912792,
+    2.193939685821533
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Geneva, Switzerland",
+    46.2214086,
+    6.139260899999954
   ]
 ];
